7ccc45ccb81ba4bba48a7c7fe18a34cb3c08c96e
Jozue06 <jozue06@gmail.com>
2018-04-14 15:57:52 -0700

    lkajsdf

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const CLIENT_URL = `https://books-josh-lina.github.io/book-list-client`;
+const CLIENT_URL = 'https://books-josh-lina.github.io/book-list-client';
 
 const DATABASE_URL = 'postgres://hoefkdtgjxbkaz:b8c1c096a186195f6cd7a669d00fd9fab02eb9e787969112e60c492331cd4a0c@ec2-54-83-19-244.compute-1.amazonaws.com:5432/d91jbt4nia47q7'
 
@@ -19,7 +19,7 @@ app.use(cors());
 app.get('/test', (req, res) => res.send('Testing 1, 2, 3'));
 
 
-app.get('/', (req, res) => res.send(CLIENT_URL));
+app.get('/', (req, res) => res.redirect(CLIENT_URL));
 
 app.get('/books', (req, res) => {
   client.query(`SELECT *  FROM books;`)
