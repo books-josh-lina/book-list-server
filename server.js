394b2f4714ba8963bf4ad96be309e0c6644d690f
Jozue06 <jozue06@gmail.com>
2018-04-14 10:28:58 -0700

    pushing to heroku

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const pg = require('pg');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const CLIENT_URL = 'https://books-josh-lina.github.io/book-list-client/';
+
+// const client = new pg.Client(process.env.DATABASE_URL);
+// client.connect();
+// client.on('error', err => console.error(err));
+
+// app.use(cors());
+
+// app.get('/test', (req, res) => res.send('Testing 1, 2, 3'));
+
+// app.get('/tasks', (req, res) => {
+//   client.query(`SELECT * from tasks;`)
+//     .then(results => res.send(results.rows))
+//     .catch(console.error);
+// });
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
