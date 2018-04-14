eecadac0faf30221ca0bfcf4bafd3515b6feee42
Laboni A <asthanal@yahoo.com>
2018-04-14 14:56:51 -0700

    tested database on local and heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const pg = require('pg');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const CLIENT_URL = process.env.'https://books-josh-lina.github.io/book-list-client';
+const CLIENT_URL = 'https://books-josh-lina.github.io/book-list-client';
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
@@ -14,14 +14,15 @@ client.on('error', err => console.error(err));
 
 app.use(cors());
 
-app.get('/test', (req, res) => res.send('Testing 1, 2, 3'));
+// app.get('/test', (req, res) => res.send('Testing 1, 2, 3'));
 
-// app.get('/books', (req, res) => {
-//   client.query(`SELECT title from books;`)
-//     .then(results => res.send(results.rows))
-//     .catch(console.error);
-// });
+app.get('/books', (req, res) => {
+  client.query(`SELECT title from books;`)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
 
 // app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
 
