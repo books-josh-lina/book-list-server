ae0ce5787aeebbfc3e4b703f32c600f1107c51a2
Laboni A <asthanal@yahoo.com>
2018-04-14 13:16:06 -0700

    added database_url in server js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.'https://books-josh-lina.github.io/book-list-client';
 
-const client = new pg.Client(process.env.postgres://hoefkdtgjxbkaz:b8c1c096a186195f6cd7a669d00fd9fab02eb9e787969112e60c492331cd4a0c@ec2-54-83-19-244.compute-1.amazonaws.com:5432/d91jbt4nia47q7);
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
