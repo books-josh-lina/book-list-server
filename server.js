4fa20fdbab79dc2cf0478482fc636ada8ef98ed0
Jozue06 <jozue06@gmail.com>
2018-04-14 18:42:31 -0700

    server update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const pg = require('pg');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const CLIENT_URL = 'https://books-josh-lina.github.io/book-list-client';
 
 const DATABASE_URL = 'postgres://hoefkdtgjxbkaz:b8c1c096a186195f6cd7a669d00fd9fab02eb9e787969112e60c492331cd4a0c@ec2-54-83-19-244.compute-1.amazonaws.com:5432/d91jbt4nia47q7'
